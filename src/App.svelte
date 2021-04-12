9797ed67fcaee7d6513ca75275ff178d41a1de23
SAMEER TARIQ <imsamtar@gmail.com>
2021-04-12 13:44:38 +0500

    day 1

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	import Blockly from "./components/Blockly.svelte";
+
+	let xml = "";
+
+	onMount(async () => {
+		const response = await fetch("/example.xml");
+		xml = await response.text();
+	});
+</script>
+
+<main>
+	{#if xml}
+		<Blockly initialXML={xml} />
+	{/if}
+</main>
+
+<style>
+	main {
+		margin: 0 auto;
+		background: #dddddd;
+		height: 100vh;
+	}
+</style>
